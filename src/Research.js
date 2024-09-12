213c3c6887e569d55c6abecb1288c53036d23445
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 05:59:41 -0400

    Update Research.js

--- src/Research.js
+++ src/Research.js
@@ -6,12 +6,12 @@ function Research() {
           <div className="container" style={{ marginTop: '20px' }}>
               <h2>Research Papers and Patent</h2>
               <p style={{ textAlign: "justify" }}>
-              PATENT: Applied for a Patent App No.: 202221000828, Title: A MEDICATION INSPECTION AND PACKAGING DEVICE at Mumbai, Intellectual Property India.<br />
-                  PAPER: Heart Arrhythmia detection through real-time ECG Acquisition by Machine Learning Techniques, presented in 12th International Conference on Soft Computing for Problem Solving 2023 and published in Lecture Notes in Networks and Systems, Springer.<br />
-                  Available at: <a href="https://doi.org/10.1007/978-981-97-3292-0_35">https://doi.org/10.1007/978-981-97-3292-0_35</a> (First Author)
+              <b>PATENT:</b> Applied for a Patent App No.: 202221000828, Title: <b>A MEDICATION INSPECTION AND PACKAGING DEVICE</b> at Mumbai, Intellectual Property India.<br />
+                  <b>PAPER:</b> Heart Arrhythmia detection through real-time ECG Acquisition by Machine Learning Techniques, presented in 12th International Conference on Soft Computing for Problem Solving 2023 and published in Lecture Notes in Networks and Systems, Springer.<br />
+                  <b>Available at:</b> <a href="https://doi.org/10.1007/978-981-97-3292-0_35">https://doi.org/10.1007/978-981-97-3292-0_35</a> (First Author)
                   <br />
-                  PAPER: Explainable predictions of different machine learning algorithms used to predict Early-Stage diabetes
-                  <br />Available at: <a href="https://arxiv.org/abs/2111.09939">https://arxiv.org/abs/2111.09939</a> (First Author)
+                  <b>PAPER:</b> Explainable predictions of different machine learning algorithms used to predict Early-Stage diabetes
+                  <br /><b>Available at:</b> <a href="https://arxiv.org/abs/2111.09939">https://arxiv.org/abs/2111.09939</a> (First Author)
               </p>
       </div>
     </section>
