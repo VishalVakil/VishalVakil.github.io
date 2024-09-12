4b926b2e02b8353a310d498a3b752cce344e0685
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 05:49:37 -0400

    Added Research exp

---
+++ src/Research_Papers_Patent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Research_Papers_Patent() {
+  return (
+    <section id="Research" className="py-5">
+          <div className="container" style={{ marginTop: '20px' }}>
+              <h2>Research Papers and Patent</h2>
+              <p style={{ textAlign: "justify" }}>
+              PATENT: Applied for a Patent App No.: 202221000828, Title: A MEDICATION INSPECTION AND PACKAGING DEVICE at Mumbai, Intellectual Property India.<br />
+                  PAPER: Heart Arrhythmia detection through real-time ECG Acquisition by Machine Learning Techniques, presented in 12th International Conference on Soft Computing for Problem Solving 2023 and published in Lecture Notes in Networks and Systems, Springer.<br />
+                  Available at: <a href="https://doi.org/10.1007/978-981-97-3292-0_35">https://doi.org/10.1007/978-981-97-3292-0_35</a> (First Author)
+                  <br />
+                  PAPER: Explainable predictions of different machine learning algorithms used to predict Early-Stage diabetes
+                  <br />Available at: <a href="https://arxiv.org/abs/2111.09939">https://arxiv.org/abs/2111.09939</a> (First Author)
+              </p>
+      </div>
+    </section>
+  );
+}
+
+export default Research_Papers_Patent;
