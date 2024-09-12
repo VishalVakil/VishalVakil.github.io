1e46b816b47f16f332a8abc8a85c12687c8497f9
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 06:27:17 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logo from './image/Logo.png'
 
 function Header() {
 
@@ -48,7 +49,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="/#"><link rel="icon" href="%PUBLIC_URL%/Logo.png" /> Vishal Vakil</a>
+        <a className="navbar-brand" href="/#"><link rel="icon" href="Logo.png" /> Vishal Vakil</a>
         <button
           className="navbar-toggler"
           type="button"
