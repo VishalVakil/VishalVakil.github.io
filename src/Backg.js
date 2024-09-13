6d8df4cf203f76d9308c04c9ee1270fcffa2520d
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 20:49:00 -0400

    TRIAL

--- src/Backg.js
+++ src/Backg.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Backg() {
     return (
-    <canvas class="partic"></canvas>
+    <canvas class="partic" style={{"background": "rgba(0,0,0,.3)",
+        "position": "fixed",
+        "zIndex": 1,
+        "top": "0",
+        "right": "0",
+        "bottom": "0",
+        "left": "0",
+        "opacity": "1",
+        "transition": "all .2s",
+        "visibility": "visible"}}></canvas>
     );
 }
 
