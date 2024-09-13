273c3da962e59623e3483b54e99070ea2e1f292c
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 21:13:10 -0400

    TRIAL 1

--- src/Backg.js
+++ src/Backg.js
@@ -3,6 +3,8 @@ import React from 'react';
 function Backg() {
     return (
     <canvas class="partic"></canvas>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js"></script>
+    <script src="%PUBLIC_URL%/script.js"></script>
     );
 }
 
