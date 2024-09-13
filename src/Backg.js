5e7b6b35930c5575185787f8991f357aa3a7b219
VishalVakil <vakil.vishal17@gmail.com>
2024-09-12 21:27:23 -0400

    Revert Changes

--- src/Backg.js
+++ src/Backg.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Backg() {
     return (
-        <>
-            <canvas className="partic"></canvas>
-        </>
+        <canvas className="partic"></canvas>
     );
 }
 
