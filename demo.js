2d2c8f1adcadc1ed4323c4432e820e8e7e1fca45
VishalVakil <vakil.vishal17@gmail.com>
2024-09-20 14:34:59 -0400

    added bg

---
+++ demo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Particles from "./Particles";
+
+export default function BasicCard() {
+    return (
+      <div>
+            <Particles />
+      </div>
+  );
+}
