acde24b663c55a953019fd4d825deb6de1d96c1b
VishalVakil <vakil.vishal17@gmail.com>
2024-09-20 23:24:28 -0400

    Common Fixes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Demo from './demo';
+import BG from './BG';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Demo />
+    <BG />
     <App />
   </React.StrictMode>
 );
