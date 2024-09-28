a15d98289e8d8d6ab1e4cc60eb68c169be229ab9
VishalVakil <vakil.vishal17@gmail.com>
2024-09-28 15:31:35 -0400

    Skills Update

--- src/Skills.js
+++ src/Skills.js
@@ -3,19 +3,45 @@ import './Skills.css';
 
 const Skills = () => {
     // Define your skills here
-    const skills = [
-        "Python", "C", "JavaScript", "C++", "Bash", "SQL", "NoSQL", "MongoDB", "PostgreSQL", "Pytorch", "NumPy", "Matplotlib", "Scikit-Learn", "Pandas", "SciPy", "Convolutional Neural Network", "Embedded Systems", "Data Visualization", "Data Analysis",
-        "Cloud Computing", "Internet of Things", "AWS", "Docker",  "Terraform", "Jenkins", "Spring Framework", "Restful API Development", "Linux OS" , "Scrum",
-        "Node.Js", "React.JS", "D3.JS", "Git", "Junit", "Rally", "JIRA", "Sonar Lint", "Confluence", "Tableau", "Power BI"
+    const ProgrammingScripting = ["Python", "C", "C++", "Assembly Language", "HTML", "JAVA", "JavaScript", "SWIFT", "Shell Scripting", "Linux/Unix", "R", "Scala", "GO", "React.js", "D3.js"];
+    //Programming & Scripting Languages
+    const DatabaseS = [
+        "PostgreSQL", "MySQL", "SQL", "MongoDB", "DynamoDB", "GraphQL", "Couchbase"
     ];
-
+    const MachineL = [
+        "PyTorch", "Tensorflow", "Conda", "NumPY", "scikit-learn", "Pandas", "OpenCV"
+    ];
+    const STT = [
+        "MATLAB", "Tableau", "UiPath", "PowerBI", "Rally", "Git", "Docker", "Spark", "Oracle", "Dialogflow", "Web Access SCADA", "AWS", "Azure", "Splunk", 
+        "GX Works3-PLC", "Keil uVision", "Internet of Things (IoT)", "Industrial Automation", "Software Development", "Machine Learning", "Artificial Intelligence", "Raspberry Pi", "Natural Language Processing (NLP)", "Robotic Process Automation (RPA)", "Jira", "Flask", "Junit", "Terraform", "Ansible", "Jenkins", "Kubernetes", "Snowflake", "Kafka"
+    ];
+    
     return (
         <section  className="py-2">
             <div className="container" id="skills">
                 <h2>Skills</h2>
-                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-                {skills.map((skill, index) => (
-                    <div key={index} className="skill">{skill}</div>
+                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <h4>Programming & Scripting Languages: </h4>
+                {ProgrammingScripting.map((skill1, index) => (
+                    <div key={index} className="skill">{skill1}</div>
+                ))}
+                </div>
+                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <h4>Database & Querying Languages: </h4>
+                {DatabaseS.map((skill2, index) => (
+                    <div key={index} className="skill">{skill2}</div>
+                ))}
+                </div>
+                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <h4>Machine Learning Framework: </h4>
+                {MachineL.map((skill3, index) => (
+                    <div key={index} className="skill">{skill3}</div>
+                ))}
+                </div>
+                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <h4>Sofware Tool & Technologies: </h4>
+                {STT.map((skill4, index) => (
+                    <div key={index} className="skill">{skill4}</div>
                 ))}
                 </div>
             </div>
