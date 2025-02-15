a4a0c53e81aa05afb4ef7a47d002e03026cce54c
VishalVakil <vakil.vishal17@gmail.com>
2025-02-15 16:46:54 -0700

    Update Experience.js
    Updated Experience

--- src/Experience.js
+++ src/Experience.js
@@ -4,12 +4,12 @@ import React from 'react';
 const Experience = () => {
   
   const tableData = [
-    ['Software Engineer (AI)', 'Padma AgRobotics', 'October 2024 - Present'],
-    ['Machine Learning Engineer', 'Naya Energy', 'August 2024 - October 2024'],
+    ['Software Engineer (AI)', 'Padma AgRobotics', 'July 2024 - Present'],
+    // ['Machine Learning Engineer', 'Naya Energy', 'August 2024 - October 2024'],
     ['Software Engineer Intern', 'Sona Data Systems', 'June 2023 - August 2023'],
     ['Software Engineer', 'General Electric Company (GE)', 'June 2021 - August 2022'],
     ['Software Engineer Intern', 'Krossmark Innovations', 'January 2021 - April 2021'],
-    ['Software Engineer Co-Op', 'Unity Pharma', 'January 2019 - December 2020']
+    ['Software Engineer Co-Op', 'Unity Pharma', 'January 2019 - February 2020']
 ];
 
   return (
