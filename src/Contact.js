9c24d469d03f07d72eeabc21809fb7a66f8643d1
VishalVakil <vakil.vishal17@gmail.com>
2025-03-06 10:23:07 -0700

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     const [status, setStatus] = useState('');
     const [showThankYou, setShowThankYou] = useState(false);
     const [formData, setFormData] = useState({
-        access_key: "9a42dcd5-5469-47a3-a734-0b607861f8c0",
+        // access_key: "9a42dcd5-5469-47a3-a734-0b607861f8c0",
         name: '',
         email: '',
         message: ''
@@ -126,7 +126,8 @@ const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setStatus('sending');
-        // formData.append("access_key", "9a42dcd5-5469-47a3-a734-0b607861f8c0");
+        const formData = new FormData(event.target);
+        formData.append("access_key", "9a42dcd5-5469-47a3-a734-0b607861f8c0");
 
         try {
             const response = await fetch('https://api.web3forms.com/submit', {
@@ -141,7 +142,7 @@ const Contact = () => {
             if (response.ok) {
                 setStatus('success');
                 setFormData({
-                    access_key: "9a42dcd5-5469-47a3-a734-0b607861f8c0",
+                    // access_key: "9a42dcd5-5469-47a3-a734-0b607861f8c0",
                     name: '',
                     email: '',
                     message: ''
